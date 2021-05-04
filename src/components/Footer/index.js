16f4ff78729cc4d4910c2cd294312632ec97efcb
Muhammad Hamza Asif <imhamzaa313@gmail.com>
2021-05-04 10:44:36 -0700

    bug fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,16 +23,16 @@ const Footer = () => {
           </ul>
         </nav>
         <div class="social">
-          <a href="#">
+          <a href="/">
             <i class="fab fa-github fa-2x"></i>
           </a>
-          <a href="#">
+          <a href="/">
             <i class="fab fa-linkedin fa-2x"></i>
           </a>
-          <a href="#">
+          <a href="/">
             <i class="fab fa-facebook fa-2x"></i>
           </a>
-          <a href="#">
+          <a href="/">
             <i class="fab fa-twitter fa-2x"></i>
           </a>
         </div>
